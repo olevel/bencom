05a3e67e983be529aac1c4a5a621a590843cb909
olevel <pcgalilio@yahoo.com>
2016-08-15 08:25:21 +0200

    stored samples list fragment and its dummy data. dummy data would be removed and replaced with stored samples data entities

--- app/src/main/java/com/hacker/olevel/tencom/MainActivity.java
+++ app/src/main/java/com/hacker/olevel/tencom/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hacker.olevel.tencom;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,15 +16,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.hacker.olevel.tencom.dummy.DummyContent;
 
+import static android.support.design.widget.NavigationView.*;
+
+public class MainActivity extends AppCompatActivity
+        implements OnNavigationItemSelectedListener, HomeFragment.OnFragmentInteractionListener, StoredSampleFragment.OnListFragmentInteractionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // This loads default HomeFragment only when there is no saved instance state
+        // That is, ensure the same fragment on screen rotation.
+        if (savedInstanceState == null) {
+            // Fragment load code
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            fragmentClass = HomeFragment.class;
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fLoaderContent, fragment).commit();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -80,22 +103,48 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Fragment fragment = null;
+        Class fragmentClass = null;
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_logout) {
+        } else if (id == R.id.nav_notifications) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_flow_test) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_samples_storage) {
+            fragmentClass = StoredSampleFragment.class;
 
-        } else if (id == R.id.nav_send) {
+        }  else if (id == R.id.nav_compressive_test) {
+
+        } else if (id == R.id.nav_bending_test) {
+
+        } else if (id == R.id.nav_profile) {
+
+        } else if (id == R.id.nav_logout) {
 
         }
+
+        // New fragment load code
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fLoaderContent, fragment).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
